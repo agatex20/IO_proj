7ac66495dcec266b9147016f5455a511dd0f215c
kamildlugajczyk <kam.dlugajczyk@gmail.com>
2020-10-26 20:30:18 +0100

    Add Application_context class (for database session)

---
+++ IOWpf/IOWpf/Models/Application_context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace IOWpf.Models
+{
+    class Application_context : DbContext
+    {
+        public DbSet<Balance> Balances { get; set; }
+        public DbSet<Piggy_bank> Piggy_Banks { get; set; }
+        public DbSet<Child> Children { get; set; }
+        public DbSet<Grown_up> Grown_Ups { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Income> Incomes { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=database.db");
+    }
+}
