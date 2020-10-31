f918472872f250d03f1ba57b05bdafbb3074f3ff
kamildlugajczyk <kam.dlugajczyk@gmail.com>
2020-10-31 12:45:06 +0100

    Add bill photo path to Expense class

--- IOWpf/IOWpf/Models/Expense.cs
+++ IOWpf/IOWpf/Models/Expense.cs
@@ -11,20 +11,18 @@ namespace IOWpf.Models
     public class Expense : Money_flow
     {
         public int ExpenseId { get; set; }
+        public string bill_photo_path { get; set; }
+
         public int? UserId { get; set; }                             // Foreign key 
         public virtual User User { get; set; }                       // Expense is associated with one User
-
-        //private ImageSource bill_photo { get; set; }
-
         public virtual ICollection<Expense_Category> Expense_Categories { get; set; }
-
+        
 
         public Expense() { }
 
-        public Expense(float amount, string creatorName, string date, string description, bool ifChilds /*ImageSource billPhoto,*/) : base(amount, creatorName, date, description, ifChilds)
+        public Expense(float amount, string creatorName, string date, string description, bool ifChilds, string bill_photo_path) : base(amount, creatorName, date, description, ifChilds)
         {
-            //bill_photo = billPhoto;
-            
+            this.bill_photo_path = bill_photo_path;
         }
 
     }
