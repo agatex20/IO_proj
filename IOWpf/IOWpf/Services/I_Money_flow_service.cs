8fc34da61520cd9df1d495d7e35b9280c6cb43b3
kamildlugajczyk <kam.dlugajczyk@gmail.com>
2020-10-31 09:34:54 +0100

    Add Services files

---
+++ IOWpf/IOWpf/Services/I_Money_flow_service.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOWpf.Services
+{
+    class I_Money_flow_service
+    {
+    }
+}
