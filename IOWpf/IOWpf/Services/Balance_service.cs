76b36d81e8b9a5e77123dbf23f5ce627541957ca
Witek <Witek@DESKTOP-TC299DS>
2020-11-05 06:47:15 +0100

    Initial balance calculating

--- IOWpf/IOWpf/Services/Balance_service.cs
+++ IOWpf/IOWpf/Services/Balance_service.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IOWpf.Migrations;
+using IOWpf.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +10,31 @@ namespace IOWpf.Services
 {
     public class Balance_service
     {
-        public float calc_balance()
+        public void calc_balance()
         {
-            return 0.0f;
+            float expsum = 0;
+            using (var db = new Application_context())
+            {
+
+                List<Expense> explist = db.Expenses.ToList();
+                List<Income> inclist = db.Incomes.ToList();
+                inclist = db.Incomes.ToList();
+                foreach (Expense x in explist)
+                {
+                    expsum -= x.amount;
+                }
+                foreach (Income x in inclist)
+                {
+                    expsum += x.amount;
+                }
+                var query = from b in db.Balances
+                            select b;
+                Balance bal = new Balance(expsum);
+
+                db.Balances.Add(bal);
+                db.SaveChanges();
+
+            }
         }
     }
 }
